0924c657ff125e955921d0302e5cf0e39f293f09
Daniel Barnes <nasonfish@nasonfish.com>
2018-05-09 14:22:51 -0600

    finish javadocs in WordGroupLocation-- remove the Comparable implementation since we're not using a PriorityQueue anymore.

--- WordGroupLocation.java
+++ WordGroupLocation.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * @author Daniel Barnes '21 Elise Glaser '20
  *
  */
-public class WordGroupLocation implements Comparable<WordGroupLocation> {
+public class WordGroupLocation {
 	
 	/**
 	 * A QueryLocation object, which stores the location and context of one of the words
@@ -14,33 +14,46 @@ public class WordGroupLocation implements Comparable<WordGroupLocation> {
 	 */
 	private final QueryLocation loc;
 	
-	
+	/**
+	 * An array containing a list of all words which can be found in {@link loc}'s file.
+	 */
 	private final ArrayList<String> words;
 	
-	
+	/**
+	 * Instantiate a WordGroupLocation. Set {@link loc} to the QueryLocation,
+	 * instantiate the WordList, and add the word which {@link loc} refers to
+	 * to the wordlist.
+	 * @param loc QueryLocation which we refer to.
+	 */
 	public WordGroupLocation(QueryLocation loc) {
 		this.loc = loc;
 		this.words = new ArrayList<String>();
 		this.words.add(loc.getWord());
 	}
 
-
+	/**
+	 * Get the location this WordGroup refers to.
+	 * @return QueryLocation
+	 */
 	public QueryLocation getLoc() {
 		return loc;
 	}
 
+	/**
+	 * Return an ArrayList of words which can be found in the location we refer to.
+	 * @return ArrayList<String>
+	 */
 	public ArrayList<String> getWords() {
 		return words;
 	}
 	
+	/**
+	 * Add a word to this WordGroup, provided it doesn't alreaday exist.
+	 * @param word String
+	 */
 	public void addWord(String word) {
 		if(!this.words.contains(word))
 			this.words.add(word);
-	}
-
-	@Override
-	public int compareTo(WordGroupLocation o) {
-		return this.words.size() - o.getWords().size();
 	}
 	
 }
