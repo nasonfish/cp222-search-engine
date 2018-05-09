e0834a769119834803df37936a7df842eb4c05eb
Daniel Barnes <nasonfish@nasonfish.com>
2018-05-09 14:18:39 -0600

    add some javadocs in wordgrouplocation

--- WordGroupLocation.java
+++ WordGroupLocation.java
@@ -1,7 +1,20 @@
 import java.util.ArrayList;
 
+/**
+ * A class to store a location associated with many words.
+ * 
+ * @author Daniel Barnes '21 Elise Glaser '20
+ *
+ */
 public class WordGroupLocation implements Comparable<WordGroupLocation> {
+	
+	/**
+	 * A QueryLocation object, which stores the location and context of one of the words
+	 * we found. All of these words in {@link words} can be found in the file {@link loc} refers to.
+	 */
 	private final QueryLocation loc;
+	
+	
 	private final ArrayList<String> words;
 	
 	
